ef8bb435a36c88e24cad63b260e15187e8ad56ad
tgronier <timothegronier@yahoo.fr>
2023-01-14 17:25:18 +0100

    Suppressed the empty folder bug when instantiating subclasses.

--- src/tools/SubclassesDictionary.py
+++ src/tools/SubclassesDictionary.py
@@ -19,14 +19,15 @@ def get_subclasses():  # this function returns all subclasses
 
         for subclass_folder in subclasses_list:  # for each subclass
             subclass_file = "lib.Subclasses." + class_name + "." + subclass_folder + "." + subclass_folder  # the file where the module is defined
-            try:
-                subclass_module = import_module(subclass_file)  # we import the .py file
-            except:
-                raise SubclassesInstantiationException(f"An error occured during the instantiation of the module {subclass_file}.\n"
-                                                       "Please check that there is a module there and that it bears the same name as the directory. If it is not the case, please add one or remove the directory from the lib/subclasses directory.")
-            for subclass_name, subclass_class in inspect.getmembers(subclass_module):
-                if inspect.isclass(subclass_class) and subclass_name != class_name:  # get back only the subclasses
-                    subclasses_dictionary[class_name][subclass_name] = subclass_class  # the subclass is added to the subclass list
+            if subclass_folder + ".py" in list_files_and_folders("lib/Subclasses/" + class_name + "/" + subclass_folder):
+                try:
+                    subclass_module = import_module(subclass_file)  # we import the .py file
+                except:
+                    raise SubclassesInstantiationException(f"An error occured during the instantiation of the module {subclass_file}.\n"
+                                                           "Please check that there is a module there and that it bears the same name as the directory. If it is not the case, please add one or remove the directory from the lib/subclasses directory.")
+                for subclass_name, subclass_class in inspect.getmembers(subclass_module):
+                    if inspect.isclass(subclass_class) and subclass_name != class_name:  # get back only the subclasses
+                        subclasses_dictionary[class_name][subclass_name] = subclass_class  # the subclass is added to the subclass list
 
     return subclasses_dictionary
 
