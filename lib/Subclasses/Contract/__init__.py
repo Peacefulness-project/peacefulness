6115903f11e2c8e66cdd0f0a7233dcf921b91cc5
tgronier <timothe.gronier@imt-atlantique.fr>
2024-04-15 19:31:14 +0200

    Beginning of the work on the modules.

--- lib/Subclasses/Contract/__init__.py
+++ lib/Subclasses/Contract/__init__.py
@@ -1,3 +1,44 @@
+from importlib import import_module  # this library allows to import modules defined as str
+from os import listdir
+import inspect
+
+from src.tools.Utilities import list_files_and_folders
+
+
+
+print(listdir("./"))
+
+
+
+# # Exception
+# class SubclassesInstantiationException(Exception):
+#     def __init__(self, message):
+#         super().__init__(message)
+#
+#
+# subclasses_dictionary = {}  # a dictionary containing all the subclasses
+#
+# classes_concerned = list_files_and_folders("lib/Subclasses/")  # the list of all classes concerned by subclasses
+#
+# for class_name in classes_concerned:
+#     subclasses_dictionary[class_name] = {}  # a sub-dictionary containing all the subclasses of the concerned class
+#     class_directory = "lib/Subclasses/" + class_name  # the directory containing subclasses of one class
+#     subclasses_list = list_files_and_folders(class_directory)  # list of all subclasses for a given class
+#
+#     for subclass_folder in subclasses_list:  # for each subclass
+#         subclass_file = "lib.Subclasses." + class_name + "." + subclass_folder + "." + subclass_folder  # the file where the module is defined
+#         if subclass_folder + ".py" in list_files_and_folders("lib/Subclasses/" + class_name + "/" + subclass_folder):
+#             subclass_module = import_module(subclass_file)  # we import the .py file
+#             try:
+#                 subclass_module = import_module(subclass_file)  # we import the .py file
+#             except:
+#                 raise SubclassesInstantiationException(f"An error occured during the instantiation of the module {subclass_file}.\n"
+#                                                        "Please check that there is a module there and that it bears the same name as the directory. If it is not the case, please add one or remove the directory from the lib/subclasses directory.")
+#             for subclass_name, subclass_class in inspect.getmembers(subclass_module):
+#                 if inspect.isclass(subclass_class) and subclass_name != class_name:  # get back only the subclasses
+#                     subclasses_dictionary[class_name][subclass_name] = subclass_class  # the subclass is added to the subclass list
+
+
 
 
 
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='peacefulness',
+      version='1.0',
+      description='toto',
+      author='toto',
+      author_email='toto',
+      url='toto',
+      # packages=['peacefulness'],
+      py_modules=['peacefulness'],
+     )
