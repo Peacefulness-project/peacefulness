d8279b182eac2ce2430c146be8c849ef6d6788f4
hallou.yasser <yasser.hallou@imt-atlantique.fr>
2024-05-28 14:25:46 +0200

    DRLstrategy est rajoutée

---
+++ interface_peacefulness.py
@@ -0,0 +1,16 @@
+# In this file, we will link peacefulness data to the Actor-Critic code
+
+# Ascending phase :
+# - from devices we should export the message which contains the formalism
+# - from devices we should also export the wanted energy resulting from the device.update() method
+# - from aggregators we should export the forecasting results through the forecaster.update_forecast() method
+
+
+# Descending phase :
+# - we should import the accorded energy to each device
+
+
+
+
+
+
